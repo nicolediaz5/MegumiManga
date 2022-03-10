0530c8061644f2f763fdbab8ee5d3931d335d1ba
Nicole Diaz <tnicolediaz@gmail.com>
2022-03-10 10:27:36 -0300

    mas categorias en el nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@
 import  Container  from "react-bootstrap/Container";
 import  Navbar  from "react-bootstrap/Navbar";
 import  Nav  from "react-bootstrap/Nav";
-
+import NavDropdown from "react-bootstrap/NavDropdown"
 
  
 
@@ -18,7 +18,13 @@ function NavBar() {
     <Nav className="me-auto">
       <Nav.Link href="#mangas">Mangas</Nav.Link>
       <Nav.Link href="#figuras">Figuras Colecionables</Nav.Link>
-      <Nav.Link href="#figuras">Comics</Nav.Link>
+      <NavDropdown title="Demográfias" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Josei</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Kodomo</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Seinen</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Shojo</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Shonen</NavDropdown.Item>
+      </NavDropdown>
       
     </Nav>
     <Nav>
