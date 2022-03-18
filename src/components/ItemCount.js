447366e2d8792055ea17b84b0c155e9266e57296
Nicole Diaz <tnicolediaz@gmail.com>
2022-03-18 14:40:38 -0300

    creacion del boton y su funcion

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,11 +1,34 @@
+
 import React from 'react'
 
+import { useState } from 'react'
+
 const ItemCount = () => {
+    const count = 1;
+    const stock = 10;
+    
+    const [cantidad, setCantidad] = useState (count);
+
+
+    const añadirProducto = (numero) => {
+        setCantidad(cantidad + numero);
+    };
+
+    const onAdd = (cantidad) => {
+      alert (`Tenes ${cantidad} productos`)
+    }
+
   return (
     <div className='d-grid gap-2 d-md-block'>
-  <button className='btn btn-primary' type="button">+</button>
-  <span className=''> </span>
-  <button className='btn btn-primary' type="button">-</button>
+
+  <button className='btn btn-primary' onClick={() => añadirProducto(+1)} disabled={cantidad === stock ? true : null} >+</button>
+  <span className=''>{cantidad}</span>
+  <button className='btn btn-primary' onClick={() => añadirProducto(-1)} disabled={cantidad === count ? true : null}>-</button>
+
+  <br/>  
+  <button className="btn btn-primary"  onClick={() => onAdd(cantidad)} disabled={stock === 0 ? true : null}>Añadir</button>
+  
+
 
 </div>
   )
