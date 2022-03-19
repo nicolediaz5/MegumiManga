fa0b7dd2c3a5d78f3b60852c9b1b88bffb9e29a6
Nicole Diaz <tnicolediaz@gmail.com>
2022-03-19 17:18:14 -0300

    añadiendo estilos

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -21,12 +21,12 @@ const ItemCount = () => {
   return (
     <div className='d-grid gap-2 d-md-block'>
 
-  <button className='btn btn-primary' onClick={() => añadirProducto(+1)} disabled={cantidad === stock ? true : null} >+</button>
-  <span className=''>{cantidad}</span>
-  <button className='btn btn-primary' onClick={() => añadirProducto(-1)} disabled={cantidad === count ? true : null}>-</button>
+  <button className=' btn btn_mas' onClick={() => añadirProducto(+1)} disabled={cantidad === stock ? true : null} >+</button>
+  <span className=' btn contenedor_cantidad'>{cantidad}</span>
+  <button className=' btn btn_menos' onClick={() => añadirProducto(-1)} disabled={cantidad === count ? true : null}>-</button>
 
   <br/>  
-  <button className="btn btn-primary"  onClick={() => onAdd(cantidad)} disabled={stock === 0 ? true : null}>Añadir</button>
+  <button className=" btn btn_add"  onClick={() => onAdd(cantidad)} disabled={stock === 0 ? true : null}>Añadir</button>
   
 
 
