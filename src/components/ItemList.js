aeb7780580d2ba3beca1bbf0db9ca0a1e96f1434
Nicole Diaz <tnicolediaz@gmail.com>
2022-03-22 13:52:31 -0300

    creacion de la promise

---
+++ src/components/ItemList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const mangas = [ 
+    {id:`1`, titulo: `Naruto`, precio: 550, picUrl: "https://cdn.normacomics.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/n/a/naruto_cast__1.jpg"},
+    {id:`2`, titulo: `Dragon Ball Z`, precio: 770, picUrl: "https://images-na.ssl-images-amazon.com/images/I/91V4lFuTzBL.jpg"},
+    {id:`3`, titulo: `Ataque a los Titanes`, precio: 720, picUrl: "https://images-na.ssl-images-amazon.com/images/I/81ZyyhTCsRL.jpg"},
+    {id:`4`, titulo: `Sakura Card Captor`, precio: 455, picUrl: "https://images-na.ssl-images-amazon.com/images/I/919mc8QlTYL.jpg"},
+    {id:`5`, titulo: `NaNa`, precio: 650, picUrl: "https://images-na.ssl-images-amazon.com/images/I/91IdgmPwo2L.jpg"},
+    {id:`6`, titulo: `Given`, precio: 360, picUrl: "https://images-na.ssl-images-amazon.com/images/I/81ZyyhTCsRL.jpg"},
+
+]
+
+const task = new Promise ((resolve, reject) => {
+    let condition = true 
+    if (condition) {
+        resolve(mangas)
+    } else {
+        reject(`No se encontraron los productos`)
+    }
+})
+
+const ItemList = () => { 
+task
+.then(resp => {
+    return resp
+})
+.catch()
+.then()
+
+  return (
+    <div>ItemList</div>
+  )
+}
+
+export default ItemList
