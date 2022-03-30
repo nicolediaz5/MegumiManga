62e13c3ef79473065f423bb28707f6cac31aafd1
Nicole Diaz <tnicolediaz@gmail.com>
2022-03-30 09:52:53 -0300

    seteando card en itemdetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-/*import ItemCount from './ItemCount'*/
+import ItemCount from './ItemCount'
 
  const ItemDetail = ({titulo, precio, descrip, picUrl}) => {
   return (
@@ -14,8 +14,8 @@ import React from 'react'
         <h5 className="card-title">{titulo}</h5>
         <h6 className="card-subtitle">${precio}</h6>
         <p className="card-text">{descrip}</p>
-        <button className="btn btn_add2">Añadir al carrito</button>
-       {/* <ItemCount />*/}
+        {/* <button className="btn btn_add2">Añadir al carrito</button>*/}
+       <ItemCount />
       </div>
     </div>
   </div>
