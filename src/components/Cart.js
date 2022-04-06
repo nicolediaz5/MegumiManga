d17fe8c712efb4cb168530c068d5b9c9a1b368a7
Nicole Diaz <tnicolediaz@gmail.com>
2022-04-06 14:56:20 -0300

    agregando firebase

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,7 +8,9 @@ const {cartList, removeCart} = useCartContext ()
   return (
     <div>
 <h1>Carrito</h1>
-         {cartList.map(item => <h2 key={item.mangaDetalle.id} > nombre: {item.mangaDetalle.titulo} precio: {item.mangaDetalle.precio}</h2>)}
+ {/*{cartList.length === 0 && <h2> Carrito Vacio !Comienza a comprar!</h2>}*/}
+
+         {cartList.map(item => <h3 key={item.mangaDetalle.id} > nombre: {item.mangaDetalle.titulo} precio: {item.mangaDetalle.precio}</h3>)}
 <button className='btn btn_add' onClick={removeCart}> Vaciar carrito</button>
     </div>
   )
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAz_miB0y3dILVA91Z1lbFaqUpCrR-UoI",
+  authDomain: "megumimanga.firebaseapp.com",
+  projectId: "megumimanga",
+  storageBucket: "megumimanga.appspot.com",
+  messagingSenderId: "340900009311",
+  appId: "1:340900009311:web:6b31f25f0760437f8452ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const getFirestoreApp = () => {
+    return app
+}
